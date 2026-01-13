21056aac63c3c7309f79d549b5799015dc01bbcc
umidrza <umidrza4@gmail.com>
2026-01-13 23:54:32 +0400

    Quest Array II

---
+++ Array/SetMismatch.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.Array;
+
+public class SetMismatch
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        int n = nums.Length;
+        bool[] seen = new bool[n + 1];
+        int duplicate = -1, missing = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (seen[nums[i]])
+                duplicate = nums[i];
+            seen[nums[i]] = true;
+        }
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (!seen[i])
+            {
+                missing = i;
+                break;
+            }
+        }
+        return new int[2] { duplicate, missing };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Leetcode.Array;
+
+var solution = new FindAllNumbersDisappearedInAnArray();
+
+var res = solution.FindDisappearedNumbers([1, 1]);
+Console.WriteLine(string.Join("", res));
+
