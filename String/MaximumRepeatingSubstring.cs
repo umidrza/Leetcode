1ca1593416c5bd385ffad19d722ffe7b163d34fd
umidrza <umidrza4@gmail.com>
2026-01-16 23:17:13 +0400

    Quest String and Queue assignments

---
+++ String/MaximumRepeatingSubstring.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.String;
+
+// https://leetcode.com/problems/maximum-repeating-substring
+public class MaximumRepeatingSubstring
+{
+    public int MaxRepeating(string sequence, string word)
+    {
+        int k = 0;
+        string repeated = word;
+
+        while (sequence.Contains(repeated))
+        {
+            k++;
+            repeated += word;
+        }
+
+        return k;
+    }
+}
