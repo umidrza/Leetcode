f7e6a7ba71ac2f8bd2b4ea0f61219eb79d1b316e
umidrza <umidrza4@gmail.com>
2026-01-17 23:18:24 +0400

    Quest Assignments

---
+++ src/Solutions/MonotonicStack/NextGreaterNodeInLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.src.Definitions;
+
+namespace Leetcode.src.Solutions.MonotonicStack;
+
+// https://leetcode.com/problems/next-greater-node-in-linked-list
+public class NextGreaterNodeInLinkedList
+{
+    public int[] NextLargerNodes(ListNode head)
+    {
+        List<int> values = new List<int>();
+        while (head != null)
+        {
+            values.Add(head.val);
+            head = head.next;
+        }
+
+        int n = values.Count;
+        int[] answer = new int[n];
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            while (stack.Count > 0 && values[i] > values[stack.Peek()])
+            {
+                answer[stack.Pop()] = values[i];
+            }
+            stack.Push(i);
+        }
+
+        return answer;
+    }
+}
---
+++ src/Solutions/PrefixSum/ContinuousSubarraySum.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.src.Solutions.PrefixSum;
+
+// https://leetcode.com/problems/continuous-subarray-sum
+public class ContinuousSubarraySum
+{
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+        var map = new Dictionary<int, int>();
+        map[0] = -1;
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            prefixSum = (prefixSum + nums[i]) % k;
+
+            if (!map.TryAdd(prefixSum, i) && i - map[prefixSum] >= 2)
+                return true;
+        }
+
+        return false;
+    }
+}
