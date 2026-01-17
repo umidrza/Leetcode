9615ec6a90d530adb00542996c725871561aa793
umidrza <umidrza4@gmail.com>
2026-01-17 15:43:42 +0400

    Quest Hash

---
+++ src/Solutions/Hash/CopyListWithRandomPointer.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.src.Definitions;
+
+namespace Leetcode.src.Solutions.Hash;
+
+// https://leetcode.com/problems/copy-list-with-random-pointer
+public class CopyListWithRandomPointer
+{
+    public Node CopyRandomList(Node head)
+    {
+        if (head == null) return null;
+        Dictionary<Node, Node> map = new Dictionary<Node, Node>();
+        Node dummy = head;
+
+        while (head != null)
+        {
+            map[head] = new Node(head.val);
+            head = head.next;
+        }
+        head = dummy;
+
+        while (head != null)
+        {
+            Node copyNode = map[head];
+            copyNode.next = head.next == null ? null : map[head.next];
+            copyNode.random = head.random == null ? null : map[head.random];
+
+            head = head.next;
+        }
+
+        return map[dummy];
+    }
+}
---
+++ src/Solutions/Hash/FirstMissingPositive.cs
@@ -0,0 +1,44 @@
+﻿namespace Leetcode.src.Solutions.Hash;
+
+// https://leetcode.com/problems/first-missing-positive
+public class FirstMissingPositiveSolution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        int n = nums.Length;
+
+        // Step 1: Move all non-positive integers to the end of the array
+        int nonPosIdx = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] <= 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[nonPosIdx];
+                nums[nonPosIdx] = temp;
+                nonPosIdx++;
+            }
+        }
+
+        // Step 2: Mark the presence of positive integers by negating the corresponding index
+        for (int i = nonPosIdx; i < n; i++)
+        {
+            int num = Math.Abs(nums[i]);
+            if (num <= n - nonPosIdx && nums[num - 1 + nonPosIdx] > 0)
+            {
+                nums[num - 1 + nonPosIdx] *= -1;
+            }
+        }
+
+        // Step 3: Find the first missing positive integer
+        for (int i = nonPosIdx; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                return i - nonPosIdx + 1;
+            }
+        }
+
+        return n - nonPosIdx + 1;
+    }
+}
