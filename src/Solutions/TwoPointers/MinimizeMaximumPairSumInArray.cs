68f13ea1cd0f3d0abd5e992af5036b28c753ebfa
umidrza <umidrza4@gmail.com>
2026-01-24 16:59:27 +0400

    Daily 01-24

---
+++ src/Solutions/TwoPointers/MinimizeMaximumPairSumInArray.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.src.Solutions.TwoPointers;
+
+// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array
+public class MinimizeMaximumPairSumInArray
+{
+    public int MinPairSum(int[] nums)
+    {
+        int n = nums.Length;
+        System.Array.Sort(nums);
+        int res = 0;
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            res = Math.Max(res, nums[i] + nums[n - i - 1]);
+        }
+
+        return res;
+    }
+}
