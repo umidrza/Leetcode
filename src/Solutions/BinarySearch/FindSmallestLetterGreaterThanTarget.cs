8e4f28f01f8b2905cc61acb663540017d66b18b9
umidrza <umidrza4@gmail.com>
2026-01-31 10:26:03 +0400

    Daily 01-31

---
+++ src/Solutions/BinarySearch/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode.src.Solutions.BinarySearch;
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target
+public class FindSmallestLetterGreaterThanTarget
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        int min = 0, max = letters.Length - 1, res = 0;
+
+        while (min <= max)
+        {
+            int mid = (min + max) / 2;
+
+            if (letters[mid] > target)
+            {
+                res = mid;
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+
+        return letters[res];
+    }
+}
