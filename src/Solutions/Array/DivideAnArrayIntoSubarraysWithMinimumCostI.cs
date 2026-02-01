fa98a218c8c24aa48879f46bc212ea227eea50e2
umidrza <umidrza4@gmail.com>
2026-02-01 17:55:40 +0400

    Daily 02-01

---
+++ src/Solutions/Array/DivideAnArrayIntoSubarraysWithMinimumCostI.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.src.Solutions.Array;
+
+// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i
+public class DivideAnArrayIntoSubarraysWithMinimumCostI
+{
+    public int MinimumCost(int[] nums)
+    {
+        int firstMin = int.MaxValue, secondMin = int.MaxValue;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < firstMin)
+            {
+                secondMin = firstMin;
+                firstMin = nums[i];
+            }
+            else if (nums[i] < secondMin)
+            {
+                secondMin = nums[i];
+            }
+        }
+        return nums[0] + firstMin + secondMin;
+    }
+}
