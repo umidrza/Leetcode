e39d9ed56ca731893682972b8b74b0185c5dd61d
umidrza <umidrza4@gmail.com>
2026-02-02 19:11:58 +0400

    Quest Graph 1

---
+++ src/Solutions/Graph/MinimumNumberOfVerticesToReachAllNodes.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.src.Solutions.Graph;
+
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes
+public class MinimumNumberOfVerticesToReachAllNodes
+{
+    public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+    {
+        var res = new List<int>();
+        bool[] canReach = new bool[n];
+
+        foreach (var edge in edges)
+        {
+            canReach[edge[1]] = true;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!canReach[i])
+                res.Add(i);
+        }
+
+        return res;
+    }
+}
