34e82d5d2efb28706f335c070c43ed8301bd543e
umidrza <umidrza4@gmail.com>
2026-02-03 17:38:38 +0400

    Daily 02-03

---
+++ src/Solutions/Array/TrionicArrayI.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.src.Solutions.Array;
+
+// https://leetcode.com/problems/trionic-array-i
+public class TrionicArrayI
+{
+    public bool IsTrionic(int[] nums)
+    {
+        int n = nums.Length, i = 0;
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        if (i == 0 || i == n - 1) return false;
+
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+        if (i == p || i == n - 1) return false;
+
+        while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+        return i == n - 1;
+    }
+}
