53a9cfc50e33d678f267315ffc23465a4ae68e11
umidrza <umidrza4@gmail.com>
2026-02-05 15:51:32 +0400

    Daily 02-05

---
+++ src/Solutions/Array/TransformedArray.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode.src.Solutions.Array;
+
+// https://leetcode.com/problems/transformed-array
+public class TransformedArray
+{
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = nums[((i + nums[i]) % n + n) % n];
+        }
+
+        return res;
+    }
+}
