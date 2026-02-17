78f6cea307737680283bd8f3b8d5f5b24ad83b1c
umidrza <umidrza4@gmail.com>
2026-02-17 10:57:55 +0400

    Bit Manipulation

---
+++ src/Solutions/BitManipulation/AddBinary.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Leetcode.src.Solutions.BitManipulation;
+
+//
+public class AddBinarySolution
+{
+    public string AddBinary(string a, string b)
+    {
+        StringBuilder res = new StringBuilder();
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int bit1 = i >= 0 ? a[i] - '0' : 0;
+            int bit2 = j >= 0 ? b[j] - '0' : 0;
+
+            int sum = bit1 + bit2 + carry;
+            res.Insert(0, sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        return res.ToString();
+    }
+}
---
+++ src/Solutions/BitManipulation/BinaryWatch.cs
@@ -0,0 +1,32 @@
+﻿namespace Leetcode.src.Solutions.BitManipulation;
+
+// https://leetcode.com/problems/binary-watch
+public class BinaryWatch
+{
+    public IList<string> ReadBinaryWatch(int turnedOn)
+    {
+        IList<string> res = new List<string>();
+
+        for (int minute = 0; minute < 60; minute++)
+        {
+            for (int hour = 0; hour < 12; hour++)
+            {
+                if (CountBits(minute) + CountBits(hour) == turnedOn)
+                    res.Add($"{hour}:{minute:D2}");
+            }
+        }
+
+        int CountBits(int num)
+        {
+            int count = 0;
+            while (num != 0)
+            {
+                num &= num - 1;
+                count++;
+            }
+            return count;
+        }
+
+        return res;
+    }
+}
