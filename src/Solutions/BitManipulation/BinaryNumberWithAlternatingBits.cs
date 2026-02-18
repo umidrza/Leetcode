0fbc8381ef063b7e8167ae94d65939ee2247f4b9
umidrza <umidrza4@gmail.com>
2026-02-18 14:46:26 +0400

    Daily 02-18

---
+++ src/Solutions/BitManipulation/BinaryNumberWithAlternatingBits.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode.src.Solutions.BitManipulation;
+
+// https://leetcode.com/problems/binary-number-with-alternating-bits
+public class BinaryNumberWithAlternatingBits
+{
+    public bool HasAlternatingBits(int n)
+    {
+        int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+}
