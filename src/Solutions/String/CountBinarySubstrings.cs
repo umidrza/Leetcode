e58a387ab4444214ebeb4dd7a482cb7eabc79c08
umidrza <umidrza4@gmail.com>
2026-02-19 16:32:38 +0400

    Daily 02-19

---
+++ src/Solutions/String/CountBinarySubstrings.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.src.Solutions.String;
+
+// https://leetcode.com/problems/count-binary-substrings
+public class CountBinarySubstringsSolution
+{
+    public int CountBinarySubstrings(string s)
+    {
+        int curr = 1, prev = 0, result = 0;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] != s[i - 1])
+            {
+                result += Math.Min(curr, prev);
+                prev = curr;
+                curr = 1;
+            }
+            else
+            {
+                curr++;
+            }
+        }
+
+        result += Math.Min(curr, prev);
+        return result;
+    }
+}
