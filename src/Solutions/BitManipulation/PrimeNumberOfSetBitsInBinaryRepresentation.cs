35383c8102971317d0719aa231e0937a5df2be55
umidrza <umidrza4@gmail.com>
2026-02-21 21:00:07 +0400

    Daily 02-21

---
+++ src/Solutions/BitManipulation/PrimeNumberOfSetBitsInBinaryRepresentation.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Leetcode.src.Solutions.BitManipulation;
+
+// https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation
+public class PrimeNumberOfSetBitsInBinaryRepresentation
+{
+    public int CountPrimeSetBits(int left, int right)
+    {
+        HashSet<int> primes = new HashSet<int> { 2, 3, 5, 7, 11, 13, 17, 19 };
+        int result = 0;
+
+        for (int i = left; i <= right; i++)
+        {
+            int bitCount = BitOperations.PopCount((uint)i);
+            if (primes.Contains(bitCount))
+                result++;
+        }
+
+        return result;
+    }
+}
