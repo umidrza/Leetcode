aca230c935cc51cd3f46a522b893c693db85a73f
umidrza <umidrza4@gmail.com>
2026-03-05 11:12:25 +0400

    Daily 03-05

---
+++ src/Solutions/String/MinimumChangesToMakeAlternatingBinaryString.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.src.Solutions.String;
+
+// https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string
+public class MinimumChangesToMakeAlternatingBinaryString
+{
+    public int MinOperations(string s)
+    {
+        char[] c = s.ToCharArray();
+        int count = 0;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (c[i] == c[i - 1])
+            {
+                c[i] = c[i] == '0' ? '1' : '0';
+                count++;
+            }
+        }
+
+        return Math.Min(count, s.Length - count);
+    }
+}
