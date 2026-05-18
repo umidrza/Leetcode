cdbb692ede223aff46ab7da6fb7aa2008396d720
Umid Rza <umidrza4@gmail.com>
2026-05-18 11:14:27 +0400

    Daily 05-17

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using Leetcode.src.Solutions.BitManipulation;
+﻿using Leetcode.src.Solutions.DFS;
 
-var solution = new BinaryGapSolution();
+var solution = new JumpGameIII();
 
-var res = solution.BinaryGap(22);
+var res = solution.CanReach([4, 2, 3, 0, 3, 1, 2], 5);
 Console.WriteLine($"[{ string.Join(" ", res)}]");
---
+++ src/Solutions/DFS/JumpGameIII.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode.src.Solutions.DFS;
+
+public class JumpGameIII
+{
+    public bool CanReach(int[] arr, int start)
+    {
+        int n = arr.Length;
+        bool[] seen = new bool[n];
+
+        bool DFS(int i)
+        {
+            if (i >= n || i < 0 || seen[i]) return false;
+            if (arr[i] == 0) return true;
+            seen[i] = true;
+
+            return DFS(i + arr[i]) || DFS(i - arr[i]);
+        }
+
+        return DFS(start);
+    }
+}
