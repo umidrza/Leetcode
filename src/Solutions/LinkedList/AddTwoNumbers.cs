8b1407edaf9704d0d9f248dbefd3fd1c034dd99d
Umid Rza <umidrza4@gmail.com>
2026-05-24 22:23:09 +0400

    Add  Two Numbers

---
+++ src/Solutions/LinkedList/AddTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using Leetcode.src.Definitions;
+
+namespace Leetcode.src.Solutions.LinkedList;
+
+public class AddTwoNumbers
+{
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
+        {
+            if (l1 == null && l2 == null && carry == 0) return null;
+
+            int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = total / 10;
+            return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
+        }
+    }
+}
