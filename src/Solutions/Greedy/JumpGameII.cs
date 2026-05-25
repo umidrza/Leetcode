8924cb9a62f238836e64135bfb1acb3508d4616f
Umid Rza <umidrza4@gmail.com>
2026-05-25 17:10:56 +0400

    Jump game 2

---
+++ src/Solutions/Greedy/JumpGameII.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.src.Solutions.Greedy;
+
+// https://leetcode.com/problems/jump-game-ii/
+public class JumpGameII
+{
+    Dictionary<int, int> memo = new Dictionary<int, int>();
+    public int Jump(int[] nums, int i = 0)
+    {
+        if (memo.ContainsKey(i)) return memo[i];
+        if (i == nums.Length - 1) return 0;
+
+        int minJumps = int.MaxValue;
+        for (int j = nums[i]; j > 0; j--)
+        {
+            if (i + j < nums.Length)
+            {
+                int jumps = Jump(nums, i + j);
+                if (jumps != int.MaxValue)
+                {
+                    minJumps = Math.Min(minJumps, jumps + 1);
+                    memo[i] = minJumps;
+                }
+            }
+        }
+
+        return minJumps;
+    }
+}
