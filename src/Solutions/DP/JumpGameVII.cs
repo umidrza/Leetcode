12dbe40c475ce5e0a978360df6c831d152e46318
Umid Rza <umidrza4@gmail.com>
2026-05-25 17:06:13 +0400

    Jump game vii

---
+++ src/Solutions/DP/JumpGameVII.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode.src.Solutions.DP;
+
+// https://leetcode.com/problems/jump-game-vii/
+public class JumpGameVII
+{
+    public bool CanReach(string s, int minJump, int maxJump)
+    {
+        int n = s.Length;
+        if (s[n - 1] != '0') return false;
+
+        bool[] dp = new bool[n];
+        dp[0] = true;
+        int reachable = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (i >= minJump && dp[i - minJump]) reachable++;
+
+            if (i > maxJump && dp[i - maxJump - 1]) reachable--;
+
+            if (reachable > 0 && s[i] == '0')
+                dp[i] = true;
+        }
+
+        return dp[n - 1];
+    }
+}
