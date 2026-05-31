6c1b6d8bf563f9941d75fe8b814e6ab5e37f039f
Umid Rza <umidrza4@gmail.com>
2026-05-31 22:34:09 +0400

    Daily 05-31

---
+++ src/Solutions/Sorting/DestroyingAsteroids.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode.src.Solutions.Sorting;
+
+// https://leetcode.com/problems/destroying-asteroids
+public class DestroyingAsteroids
+{
+    public bool AsteroidsDestroyed(int mass, int[] asteroids)
+    {
+        System.Array.Sort(asteroids);
+        long currMass = mass;
+
+        foreach (int asteroid in asteroids)
+        {
+            if (asteroid > currMass)
+                return false;
+
+            currMass += asteroid;
+        }
+
+        return true;
+    }
+}
