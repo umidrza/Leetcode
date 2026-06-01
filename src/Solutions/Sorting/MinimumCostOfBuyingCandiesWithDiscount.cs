787b6cb328c45c00b7b5fee1575be7c725313c8b
Umid Rza <umidrza4@gmail.com>
2026-06-01 10:41:39 +0400

    Daily 06-01

---
+++ src/Solutions/Sorting/MinimumCostOfBuyingCandiesWithDiscount.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode.src.Solutions.Sorting;
+
+// https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount
+public class MinimumCostOfBuyingCandiesWithDiscount
+{
+    public int MinimumCost(int[] cost)
+    {
+        System.Array.Sort(cost);
+        System.Array.Reverse(cost);
+        int res = 0;
+
+        for (int i = 0; i < cost.Length; i++)
+        {
+            if ((i + 1) % 3 == 0) continue;
+            res += cost[i];
+        }
+
+        return res;
+    }
+}
