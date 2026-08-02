925f1b11367fa7a8d0a129ecef33d694545034f7
Umid Rza <umidrza4@gmail.com>
2026-08-02 11:20:30 +0400

    Predict the winner

---
+++ src/Solutions/Recursion/PredictTheWinnerSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode.src.Solutions.Recursion;
+
+// https://leetcode.com/problems/predict-the-winner
+public class PredictTheWinnerSolution
+{
+    public bool PredictTheWinner(int[] nums)
+    {
+        return Play(0, nums.Length - 1, 0, 0, true, nums);
+    }
+
+    bool Play(int left, int right, int player1, int player2, bool turn, int[] nums)
+    {
+        if (left > right)
+            return player1 >= player2;
+
+        if (turn)
+        {
+            return Play(left + 1, right, player1 + nums[left], player2, false, nums) ||
+                   Play(left, right - 1, player1 + nums[right], player2, false, nums);
+        }
+        else
+        {
+            return Play(left + 1, right, player1, player2 + nums[left], true, nums) &&
+                   Play(left, right - 1, player1, player2 + nums[right], true, nums);
+        }
+    }
+}
