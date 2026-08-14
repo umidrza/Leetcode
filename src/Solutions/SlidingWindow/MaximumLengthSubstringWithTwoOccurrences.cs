a2f6f0f2737de11f1103745ae33dfbaada5d026d
Umid Rza <umidrza4@gmail.com>
2026-08-14 16:57:43 +0400

    Daily 08-14

---
+++ src/Solutions/SlidingWindow/MaximumLengthSubstringWithTwoOccurrences.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.src.Solutions.SlidingWindow;
+
+// https://leetcode.com/problems/maximum-length-substring-with-two-occurrences
+public class MaximumLengthSubstringWithTwoOccurrences
+{
+    public int MaximumLengthSubstring(string s)
+    {
+        int[] freq = new int[26];
+        int res = 0;
+
+        int l = 0;
+        for (int r = 0; r < s.Length; r++)
+        {
+            int c = s[r] - 'a';
+            freq[c]++;
+
+            while (l < r && freq[c] > 2)
+            {
+                freq[s[l] - 'a']--;
+                l++;
+            }
+
+            res = Math.Max(r - l + 1, res);
+        }
+
+        return res;
+    }
+}
