98453df0ea4ebafc076f7012d45f756fbf33a795
Umid Rza <umidrza4@gmail.com>
2026-09-14 10:15:29 +0400

    Daily 09-14

---
+++ src/Solutions/Math Problems/RectangleOverlap.cs
@@ -0,0 +1,12 @@
+namespace Leetcode.src.Solutions.Math_Problems;
+
+// https://leetcode.com/problems/rectangle-overlap
+public class RectangleOverlap
+{
+    public bool IsRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec1[0] < rec2[2] &&
+               rec2[0] < rec1[2] &&
+               rec1[1] < rec2[3] &&
+               rec2[1] < rec1[3];
+    }
+}
